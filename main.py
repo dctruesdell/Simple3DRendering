fc2780114b2bb8b607f507d64912247f01d11e67
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2025-02-26 19:47:06 -0600

    updated game logic to draw an octahedron to the screen

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame as pg
+import pygame.math as pgm
+from Polyhedron import Octahedron
+from Camera import Camera
 
 
 RESOLUTION = 1000, 600
 BG_COLOR = "black"
+FRAME_RATE = 30
+
+OCTAHEDRON = Octahedron(pgm.Vector3(0, 0, 0), 30, (0, 0, 255))
+
 
 
 def main():
     pg.init()
     screen = pg.display.set_mode(RESOLUTION)
     clock = pg.time.Clock()
+    camera = Camera(pgm.Vector3(0, 0, 0), 180, screen)
+
     running = True
+    OCTAHEDRON.translate(pgm.Vector3(0, 0, 50))
 
     while running:
         for event in pg.event.get():
@@ -17,8 +27,9 @@ def main():
                 running = False
 
         screen.fill(BG_COLOR)
-
+        camera.draw_polyhedron(OCTAHEDRON)
         pg.display.flip()
+        clock.tick(FRAME_RATE)
 
 
 if __name__ == "__main__":
