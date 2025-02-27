3af1964bfb3faa5c180c73b98321e4671b7f7608
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2025-02-26 20:25:25 -0600

    added 3 axis rotation function

--- Polyhedron.py
+++ Polyhedron.py
@@ -1,6 +1,13 @@
+import math
+
 import pygame.math as pgm
 import pygame as pg
-import typing
+from enum import Enum
+
+class Axis(Enum):
+    X = "x"
+    Y = "y"
+    Z = "z"
 
 
 class Polyhedron:
@@ -14,6 +21,35 @@ class Polyhedron:
 
     def translate(self, translation_vector: pgm.Vector3) -> None:
         self.position += translation_vector
+
+    def rotate(self, axis: Axis, angle):
+        new_verts = []
+        cos_angle = math.cos(angle)
+        sin_angle = math.sin(angle)
+        if axis == Axis.X:
+            for vertex in self.vertices:
+                y = (vertex.y * cos_angle) - (vertex.z * sin_angle)
+                z = (vertex.y * sin_angle) + (vertex.z * cos_angle)
+                new_verts.append(pgm.Vector3(vertex.x, y, z))
+        self.vertices = new_verts
+
+        if axis == Axis.Y:
+            for vertex in self.vertices:
+                x = (vertex.x * cos_angle) + (vertex.z * sin_angle)
+                z = (vertex.z * cos_angle) - (vertex.y * sin_angle)
+                new_verts.append(pgm.Vector3(x, vertex.y, z))
+        self.vertices = new_verts
+
+        if axis == Axis.Z:
+            for vertex in self.vertices:
+                x = (vertex.x * cos_angle) - (vertex.y * sin_angle)
+                y = (vertex.y * sin_angle) + (vertex.y * cos_angle)
+                new_verts.append(pgm.Vector3(x, y, vertex.z))
+        self.vertices = new_verts
+
+    # shape[i].y = (shape[i].y * rcos(deg)) - (shape[i].z * rsin(deg))
+    # shape[i].z = (shape[i].y * rsin(deg)) + (shape[i].z * rcos(deg))
+
 
     def scale(self, factor) -> None:
         new_verts = []
