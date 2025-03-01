fb346c70dc5883f5c792827df513bffb8d0cee12
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2025-02-28 19:45:45 -0600

    all draw steps now look for SHAPES[active_shape]

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ FRAME_RATE = 30
 OCTAHEDRON = Octahedron(Vector3(256, 256, 0), 100, (0, 0, 255))
 CUBE = Cube(Vector3(256, 256, 0), 100, (255, 0, 0))
 SHAPES = [OCTAHEDRON, CUBE]
-active_shape = SHAPES[1]
+
 ROTATE_AMOUNT = 1
 
 
@@ -22,36 +22,44 @@ def main():
     screen = pg.display.set_mode(RESOLUTION)
     clock = pg.time.Clock()
     camera = Camera(pgm.Vector3(0, 0, 0), 180, screen)
+    active_shape = 0
 
     running = True
-    active_shape.translate(pgm.Vector3(0, 0, 50))
+    SHAPES[active_shape].translate(pgm.Vector3(0, 0, 50))
 
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
 
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_c:
+                    active_shape += 1
+                    if active_shape > len(SHAPES):
+                        active_shape = 0
+
         # reads full keyboard
         keys_down = pg.key.get_pressed()
         if keys_down[pg.K_UP]:
-            active_shape.rotate(Axis.X, ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.X, ROTATE_AMOUNT, True)
         elif keys_down[pg.K_DOWN]:
-            active_shape.rotate(Axis.X, -ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.X, -ROTATE_AMOUNT, True)
 
         if keys_down[pg.K_RIGHT]:
-            active_shape.rotate(Axis.Y, ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.Y, ROTATE_AMOUNT, True)
         elif keys_down[pg.K_LEFT]:
-            OCTAHEDRON.rotate(Axis.Y, -ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.Y, -ROTATE_AMOUNT, True)
 
         if keys_down[pg.K_x]:
-            active_shape.rotate(Axis.Z, ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.Z, ROTATE_AMOUNT, True)
         elif keys_down[pg.K_z]:
-            active_shape.rotate(Axis.Z, -ROTATE_AMOUNT, True)
+            SHAPES[active_shape].rotate(Axis.Z, -ROTATE_AMOUNT, True)
+
 
         # draw step
         screen.fill(BG_COLOR)
 
-        camera.draw_polyhedron_wireframe(active_shape, 1)
+        camera.draw_polyhedron_wireframe(SHAPES[active_shape], 1)
         pg.display.flip()
         clock.tick(FRAME_RATE)
 
