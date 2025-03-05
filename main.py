dcbedee9e56d2b14032016c95d659ead91cd825e
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2025-03-04 20:54:12 -0600

    the camera's focal length is now defined as a const FOCAL_LEN instead of being hard-coded as an int. Adjusted the position of OCTAHEDRON

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from Camera import Camera
 RESOLUTION = 1000, 600
 BG_COLOR = "black"
 FRAME_RATE = 30
+FOCAL_LEN = 270
 
-OCTAHEDRON = Octahedron(Vector3(256, 256, 0), 100, (0, 0, 255))
+OCTAHEDRON = Octahedron(Vector3(256, 256, 0), 1000, (0, 0, 255))
 CUBE = Cube(Vector3(256, 256, 0), 100, (255, 0, 0))
 SHAPES = [OCTAHEDRON, CUBE]
 
@@ -20,11 +21,11 @@ def main():
     pg.init()
     screen = pg.display.set_mode(RESOLUTION)
     clock = pg.time.Clock()
-    camera = Camera(Vector3(0, 0, 0), 180, screen)
+    camera = Camera(Vector3(0, 0, 0), FOCAL_LEN, screen)
     active_shape = 0
 
     running = True
-    SHAPES[active_shape].translate(Vector3(0, 0, 50))
+    SHAPES[active_shape].translate(Vector3(0, 0, 1))
 
     while running:
         for event in pg.event.get():
@@ -57,7 +58,6 @@ def main():
             SHAPES[active_shape].rotate(Axis.Z, -ROTATE_AMOUNT, True)
 
         # -- camera movement --
-
         if keys_down[pg.K_w]:
             camera.translate(Vector3(0, 0, MOVE_SPEED))
         elif keys_down[pg.K_s]:
