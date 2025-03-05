5a63911f9bffd7117ccffb4e0d00dab0e80f5e19
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2025-03-04 21:14:52 -0600

    main() now checks for imports before starting the program

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+import mainloop
+
 
 def main():
-    pass
+    print("-" * 20)
+    print("Simple3DRenderer")
+    print("By Diana Truesdell")
+    print("2025")
+    print("-" * 20)
 
+    load_success = True
+
+    try:
+        import pygame
+    except ModuleNotFoundError as error:
+        print(error)
+        print("Pygame is required! Read README.md for installation instructions.")
+        load_success = False
+    try:
+        import Camera
+        import Polyhedron
+        import Object3D
+    except ModuleNotFoundError as error:
+        print(error)
+        print("One or more Python files are missing. Visit http://github.com/dctruesdell/Simple3DRendering "
+              "to download source code.")
+
+    if not load_success:
+        print("Unable to load.")
+        return
+    input("Press enter to continue")
+    mainloop.main_loop()
 
 if __name__ == "__main__":
     main()
